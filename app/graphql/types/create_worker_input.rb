72eca18503ce29d06525ba9f13ed80453b5dd900
Egor Pavlov <pavlov_egor007@mail.ru>
2021-09-02 19:09:16 +0300

    fix create_worker_input

--- app/graphql/types/create_worker_input.rb
+++ app/graphql/types/create_worker_input.rb
@@ -1,6 +1,7 @@
 module Types
   class CreateWorkerInput < Types::BaseInputObject
     argument :user_id, ID, required: true
+    argument :public_catering_id, ID, required: true
     argument :role, String, required: true
     argument :rating, Float, required: false
     argument :salary, Float, required: false
